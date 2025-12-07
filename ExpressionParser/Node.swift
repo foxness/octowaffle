067c8c1be6e938621f688b5fa43c3daa01ef3183
River <foxnezz@gmail.com>
2025-12-07 21:55:58 +0300

    [ADD] add string representation to node

--- ExpressionParser/Node.swift
+++ ExpressionParser/Node.swift
@@ -14,3 +14,32 @@ enum Value: Equatable {
     case number(Double)
     case node(Node)
 }
+
+extension Value: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case let .number(number):
+            return String(number)
+        case let .node(node):
+            return "\(node)"
+        }
+    }
+}
+
+extension Node: CustomStringConvertible {
+    var description: String {
+        let operationSymbol: String
+        switch operation {
+        case .addition:
+            operationSymbol = "+"
+        case .subtraction:
+            operationSymbol = "-"
+        case .multiplication:
+            operationSymbol = "*"
+        case .division:
+            operationSymbol = "/"
+        }
+        
+        return "(\(children[0]) \(operationSymbol) \(children[1]))"
+    }
+}
